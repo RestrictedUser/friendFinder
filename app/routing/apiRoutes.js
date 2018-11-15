86745e4c23648531eb5e26becf79e95be8e9d2a4
RestrictedTM <tylermiddleton60@gmail.com>
2018-11-14 16:34:49 -0800

    minor changes and some comments added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
 
     // default friend match is the first friend but result will be whoever has the minimum difference in scores
     var bestFriendIndex = 0;
-    var minimumDifference = 40;
+    var minimumDifference = 30;
 
     // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
     //  whatever the difference is, add to the total difference
@@ -31,13 +31,19 @@ module.exports = function(app) {
       }
 
       // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      // When loop hits a friend that qualifies the bestFriendsIndex recives a value = i which represents an index in friends array.
+      // if totalDiff is less then the previous friend then set minDiff = lowest totalDiff
+      
+      // ex. index i hits friends[2]=John and gets a totalDiff = 25 which is less then minDiff defualt so John becomes current bestFriend and the new minDiff = 25
+      //ex. index i hits friends[3] Dave and gets a totalDiff = 20 which is less then Johns totalDiff so Dave becomes the new current bestFriend and the new minDiff = 20.
       if(totalDifference < minimumDifference) {
+        // value givin to i represents the bestFriend that will be chosen from friends[i]
         bestFriendIndex = i;
         minimumDifference = totalDifference;
       }
     }
 
-    // after finding match, add user to friend array
+    // after finding match, add user to friend array. So that the current users info can be used to match bestFriends with a new user
     friends.push(user);
 
     // send back to browser the best friend match
